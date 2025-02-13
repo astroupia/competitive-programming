525bb6d56095b994f3c6211fda9c41aeb38f69fd
Nahom Teguade Kassa <103548612+astroupia@users.noreply.github.com>
2025-02-13 06:39:41 +0300

    Created 'X-Sum' by A2SV Hub

---
+++ 13-Feb-2025/X-Sum 261727.py
@@ -0,0 +1,31 @@
+# Problem: X-Sum - https://codeforces.com/problemset/problem/1676/D
+
+from collections import Counter
+
+t = int(input())
+
+for _ in range(t):
+    m, n = map(int, input().split())
+
+    chess = []
+
+    for i in range(m):
+        chess.append(list(map(int, input().split())))
+
+    count_main = Counter()
+    count_anti = Counter()
+
+    for i in range(m):
+        for j in range(n):
+            count_main[i + j] += chess[i][j]
+            count_anti[i-j] += chess[i][j]
+        
+    sum = 0
+    for i in range(m):
+        for j in range(n):
+            main_sum = count_main[i + j] - chess[i][j]
+            anti_sum = count_anti[i - j]
+            sum = max(sum, main_sum + anti_sum) 
+    
+
+    print(sum)
