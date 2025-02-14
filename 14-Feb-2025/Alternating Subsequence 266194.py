dd4f6694f0f7b96523d7bd54cea64b7252a36a79
Nahom Teguade Kassa <103548612+astroupia@users.noreply.github.com>
2025-02-14 18:25:56 +0300

    Created 'Alternating Subsequence' by A2SV Hub

---
+++ 14-Feb-2025/Alternating Subsequence 266194.py
@@ -0,0 +1,23 @@
+# Problem: Alternating Subsequence - https://codeforces.com/contest/1343/problem/C
+
+t = int(input())
+
+for _ in range(t):
+    num = int(input())
+    nums = list(map(int, input().split()))
+    
+    result = [nums[0]]
+
+    for ptr in range(1, len(nums)):
+        if nums[ptr] < 0:
+            if result[-1] <= nums[ptr] and result[-1] < 0:
+                result[-1] = nums[ptr]
+            elif result[-1] > 0: 
+                result.append(nums[ptr])
+        else:
+            if result[-1] > 0 and nums[ptr] >= result[-1]:
+                result[-1] = nums[ptr]
+            elif result[-1] < 0:
+                result.append(nums[ptr])
+                
+    print(sum(result))
