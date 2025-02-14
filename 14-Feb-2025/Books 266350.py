e0348bd586de9aba5cb8efef6de322773102c8bc
Nahom Teguade Kassa <103548612+astroupia@users.noreply.github.com>
2025-02-14 19:45:45 +0300

    Created 'Books' by A2SV Hub

---
+++ 14-Feb-2025/Books 266350.py
@@ -0,0 +1,17 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+
+left, sum_time, result = 0, 0, 0
+
+for right in range(len(nums)):
+    sum_time += nums[right]
+
+    while sum_time > m:
+        sum_time -= nums[left]
+        left += 1
+    
+    result = max(result, right - left + 1)
+
+print(result)
