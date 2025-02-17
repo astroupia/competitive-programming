ad42da163c2b1b6f9c9e9798038f4dc95f35f46b
Nahom Teguade Kassa <103548612+astroupia@users.noreply.github.com>
2025-02-17 11:33:39 +0300

    Created 'F - Array Transformation' by A2SV Hub

---
+++ A2SV G6 - Round #2 17-Feb-2025/F - Array Transformation 268378.py
@@ -0,0 +1,26 @@
+# Problem: F - Array Transformation - https://codeforces.com/gym/586960/problem/F
+
+from collections import Counter
+
+def min_operations(arr):
+    n = len(arr)
+    freq = Counter(arr)
+    max_count = max(freq.values())
+    count_map = {i: 0 for i in range(1, max_count + 1)}
+
+    for value in freq.values():
+        for i in range(1, value + 1):
+            count_map[i] += 1
+
+    result = float('inf')
+    for key, value in count_map.items():
+        result = min(result, n - value * key)
+
+    return result
+
+t = int(input())
+
+for _ in range(t):
+    _ = int(input())
+    arr = list(map(int, input().split()))
+    print(min_operations(arr))
