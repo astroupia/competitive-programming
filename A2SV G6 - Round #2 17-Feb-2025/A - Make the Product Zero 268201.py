7cd0ad18cf33e547b2cbdc17a53823138d0faee5
Nahom Teguade Kassa <103548612+astroupia@users.noreply.github.com>
2025-02-17 10:05:56 +0300

    Created 'A - Make the Product Zero' by A2SV Hub

---
+++ A2SV G6 - Round #2 17-Feb-2025/A - Make the Product Zero 268201.py
@@ -0,0 +1,14 @@
+# Problem: A - Make the Product Zero - https://codeforces.com/gym/586960/problem/A
+
+from math import inf
+
+length = int(input())
+nums = list(map(int, input().split()))
+
+if 0 in nums:
+    print(0)
+else:
+    min_num  = float(inf)
+    for num in nums:
+        min_num = min(min_num, abs(num))
+    print(min_num)
