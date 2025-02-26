7d9c653739f2b48f07f34c8bbe399d1417a85526
Nahom Teguade Kassa <103548612+astroupia@users.noreply.github.com>
2025-02-26 19:02:14 +0300

    Created 'D - Minimize the Distance' by A2SV Hub

---
+++ A2SV G6 - Round #4 26-Feb-2025/D - Minimize the Distance 285090.py
@@ -0,0 +1,7 @@
+# Problem: D - Minimize the Distance - https://codeforces.com/gym/590053/problem/D
+
+num = int(input())
+
+nums = list(map(int, input().split()))
+nums.sort()
+print(nums[(num - 1) // 2])
