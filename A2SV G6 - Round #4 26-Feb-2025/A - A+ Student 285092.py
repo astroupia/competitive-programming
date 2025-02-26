061410a66625c023734f3dde2eb374b560971e52
Nahom Teguade Kassa <103548612+astroupia@users.noreply.github.com>
2025-02-26 19:02:31 +0300

    Created 'A - A+ Student' by A2SV Hub

---
+++ A2SV G6 - Round #4 26-Feb-2025/A - A+ Student 285092.py
@@ -0,0 +1,12 @@
+# Problem: A - A+ Student - https://codeforces.com/gym/590053/problem/A
+
+t = int(input())
+
+for _ in range(t):
+    nums = list(map(int, input().split()))
+    
+    student_a = max(0, max(nums[1], nums[2]) + 1 - nums[0])
+    student_b = max(0, max(nums[0],  nums[2]) + 1 - nums[1])
+    student_c = max(0, max(nums[0], nums[1]) + 1 - nums[2])
+
+    print(student_a, student_b, student_c)
