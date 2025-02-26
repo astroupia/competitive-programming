3fa3bf7ec805ff83d1a2ac931ff8911df3ec58d3
Nahom Teguade Kassa <103548612+astroupia@users.noreply.github.com>
2025-02-26 19:01:59 +0300

    Created 'B - Jo's Adventure' by A2SV Hub

---
+++ A2SV G6 - Round #4 26-Feb-2025/B - Jo's Adventure 285088.py
@@ -0,0 +1,24 @@
+# Problem: B - Jo's Adventure - https://codeforces.com/gym/590053/problem/B
+
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+
+prefix = [0] * n
+sufix = [0] * n
+
+for i in range(1, n):
+    prefix[i] = prefix[i - 1] + max(0, nums[i - 1] - nums[i])
+
+for i in range(1, n):
+    sufix[i] = sufix[i - 1] + max(0, nums[i] - nums[i - 1])
+
+for _ in range(m):
+    p, q = map(int, input().split())
+
+    p -= 1
+    q -= 1
+    
+    if p < q:
+        print(prefix[q] - prefix[p])
+    else:
+        print(sufix[p] - sufix[q])
