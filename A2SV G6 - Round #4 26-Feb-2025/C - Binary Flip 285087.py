ebbbcef7628722b4053b9f6420df5fee9a28ddf7
Nahom Teguade Kassa <103548612+astroupia@users.noreply.github.com>
2025-02-26 19:01:36 +0300

    Created 'C - Binary Flip' by A2SV Hub

---
+++ A2SV G6 - Round #4 26-Feb-2025/C - Binary Flip 285087.py
@@ -0,0 +1,34 @@
+# Problem: C - Binary Flip - https://codeforces.com/gym/590053/problem/C
+
+t = int(input())
+
+for _ in range(t):
+    num = int(input())
+    first_nums = list(map(int, input().strip()))
+    second_nums = list(map(int, input().strip()))
+    left = - 1 
+    diff = count = 0
+    flag = True
+
+    for r in range(len(first_nums)):
+        if  first_nums[r] != second_nums[r]:
+            diff += 1
+        
+        if first_nums[r] == 1:
+            count += 1
+        
+        if count * 2 == r + 1:
+            if diff != 0 and diff != r - left:
+                flag = False
+                break
+            diff = 0
+            left = r
+ 
+    if diff != 0 and diff != num - left:
+        flag = False
+
+    if flag:
+        print("YES")
+    else:
+        print("NO")
+    
