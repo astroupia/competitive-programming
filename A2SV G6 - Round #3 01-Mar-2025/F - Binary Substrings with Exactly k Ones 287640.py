5c3e060f2cefa75559c133e8bd4485e40f2a5d25
Nahom Teguade Kassa <103548612+astroupia@users.noreply.github.com>
2025-03-01 16:53:48 +0300

    Created 'F - Binary Substrings with Exactly k Ones' by A2SV Hub

---
+++ A2SV G6 - Round #3 01-Mar-2025/F - Binary Substrings with Exactly k Ones 287640.py
@@ -0,0 +1,21 @@
+# Problem: F - Binary Substrings with Exactly k Ones - https://codeforces.com/gym/588468/problem/F
+
+from collections import defaultdict
+
+seen = defaultdict(int)
+seen[0] = 1
+k = int(input())
+binary = input()
+
+res = 0
+running_sum = 0
+
+for i, num in enumerate(binary):
+    running_sum += int(num)
+
+    if running_sum - k in seen:
+        res += seen[running_sum - k]
+    
+    seen[running_sum] += 1
+
+print(res)
