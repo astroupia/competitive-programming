b306d295e48fd7741d040d11468a3c5179c20569
Nahom Teguade Kassa <103548612+astroupia@users.noreply.github.com>
2025-03-02 23:14:49 +0300

    Created 'C - Minimal TV Subscriptions' by A2SV Hub

---
+++ A2SV G6 - Round #3 02-Mar-2025/C - Minimal TV Subscriptions 288418.py
@@ -0,0 +1,20 @@
+# Problem: C - Minimal TV Subscriptions - https://codeforces.com/gym/588468/problem/C
+
+t = int(input())
+for _ in range(t):
+    n, k, d = map(int, input().split())
+    a = list(map(int, input().split()))
+    freq = {}
+    for i in range(d):
+        freq[a[i]] = freq.get(a[i], 0) + 1
+    min_subscriptions = len(freq)
+    for i in range(d, n):
+        left = a[i - d]
+        freq[left] -= 1
+        if freq[left] == 0:
+            del freq[left]
+        right = a[i]
+        freq[right] = freq.get(right, 0) + 1
+        unique_shows = len(freq)
+        min_subscriptions = min(min_subscriptions, unique_shows)
+    print(min_subscriptions)
