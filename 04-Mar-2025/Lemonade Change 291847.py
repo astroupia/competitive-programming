789a3b0b209a303a6bd66a53f4b5d5383664ad75
Nahom Teguade Kassa <103548612+astroupia@users.noreply.github.com>
2025-03-04 19:13:39 +0300

    Created 'Lemonade Change' by A2SV Hub

---
+++ 04-Mar-2025/Lemonade Change 291847.py
@@ -0,0 +1,24 @@
+# Problem: Lemonade Change - https://leetcode.com/problems/lemonade-change/
+
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        bill_count = defaultdict(int)
+
+        for i in range(len(bills)):
+            bill_count[bills[i]] += 1
+        
+            if bills[i] == 10:
+                if bill_count[5] > 0:
+                    bill_count[5] -= 1
+                else:
+                    return False
+            elif bills[i] == 20:
+                if bill_count[10] > 0 and bill_count[5] > 0:
+                    bill_count[10] -= 1
+                    bill_count[5] -= 1
+                elif bill_count[5] >= 3:
+                    bill_count[5] -= 3
+                else:
+                    return False
+                
+        return True
