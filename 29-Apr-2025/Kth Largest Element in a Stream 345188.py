2d2f812b5b6d664b751eb70d1910f2abd07fb52b
Nahom Teguade Kassa <103548612+astroupia@users.noreply.github.com>
2025-04-30 02:49:16 +0300

    Created 'Kth Largest Element in a Stream' by A2SV Hub

---
+++ 29-Apr-2025/Kth Largest Element in a Stream 345188.py
@@ -0,0 +1,18 @@
+# Problem: Kth Largest Element in a Stream - https://leetcode.com/problems/kth-largest-element-in-a-stream/
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: list[int]):
+        self.k = k
+        self.min_heap = nums
+        heapq.heapify(self.min_heap)
+
+        # Reduce heap to size k
+        while len(self.min_heap) > k:
+            heapq.heappop(self.min_heap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.min_heap, val)
+        if len(self.min_heap) > self.k:
+            heapq.heappop(self.min_heap)
+        return self.min_heap[0]
