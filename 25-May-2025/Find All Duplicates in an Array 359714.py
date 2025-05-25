20271a49eccfd0afe4bbfac07a55c40685765a2f
Nahom Teguade Kassa <103548612+astroupia@users.noreply.github.com>
2025-05-25 20:56:09 +0300

    Created 'Find All Duplicates in an Array' by A2SV Hub

---
+++ 25-May-2025/Find All Duplicates in an Array 359714.py
@@ -0,0 +1,5 @@
+# Problem: Find All Duplicates in an Array - https://leetcode.com/problems/find-all-duplicates-in-an-array/description/
+
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        return [key for key, value in Counter(nums).most_common() if value == 2]
